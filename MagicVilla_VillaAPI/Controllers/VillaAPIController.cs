fc5cdecbaf69877ad7e339ccec245ca0f49721d6
haseebrao1996 <haseebrao1996@gmail.com>
2023-09-03 18:23:30 +0500

    commit 2

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -2,7 +2,9 @@
 using MagicVilla_VillaAPI.Model;
 using MagicVilla_VillaAPI.Model.dto;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Linq.Expressions;
+using System.Text.Json;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
@@ -14,6 +16,22 @@ namespace MagicVilla_VillaAPI.Controllers
         public IEnumerable<VillaDto> GetVillas()
         {
             return VillaStore.list;
+
+        }
+        [HttpGet("id")]
+        public IActionResult GetVilla(int id)
+        {
+
+            VillaDto data=VillaStore.list.Where(x => x.Id == id).FirstOrDefault();
+            if (data != null)
+            {
+              
+                return Content(JsonConvert.SerializeObject(data));
+            }
+            else
+            {
+                return NotFound("Resource Not Found");
+            }
 
         }
     }
